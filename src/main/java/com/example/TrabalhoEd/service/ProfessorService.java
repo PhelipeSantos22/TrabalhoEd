697f688e758b88e489df13c46312379137dc287b
PhelipeSantos22 <Phelipescion@gmail.com>
2024-11-13 11:34:44 -0300

    lista encadeada
    lista encadeada adicionada ao professor

--- src/main/java/com/example/TrabalhoEd/service/ProfessorService.java
+++ src/main/java/com/example/TrabalhoEd/service/ProfessorService.java
@@ -1,5 +1,6 @@
 package com.example.TrabalhoEd.service;
 
+import com.example.TrabalhoEd.model.ListaEncadeada;
 import com.example.TrabalhoEd.model.Professor;
 import org.springframework.stereotype.Service;
 
@@ -13,7 +14,19 @@ import java.nio.file.Paths;
 
 @Service
 public class ProfessorService {
+    private final ListaEncadeada<Professor> listaProfessor;
     private static final String PROFESSORES_FILE = "src/main/resources/Professores.csv";
+
+    public ProfessorService() {
+        this.listaProfessor = new ListaEncadeada<>(
+                PROFESSORES_FILE,
+                linha -> {
+                    String[] campos = linha.split(",");
+                    return new Professor(campos[0], campos[1], campos[2], campos[3] );
+                },
+                professor -> professor.getCpf() + "," + professor.getNome() + "," + professor.getAreaDeConhecimento() + "," + professor.getPontos()
+        );
+    }
 
     public void inserirProfessor(Professor professor) {
         try (FileWriter writer = new FileWriter(PROFESSORES_FILE, true)) {
@@ -43,45 +56,12 @@ public class ProfessorService {
         return null; // Retorna null se não encontrar o professor
     }
 
-    public void atualizarProfessor(Professor professor) {
-        Path caminhoArquivo = Paths.get(PROFESSORES_FILE);
-        try {
-            var linhas = Files.readAllLines(caminhoArquivo);
-            try (FileWriter writer = new FileWriter(PROFESSORES_FILE)) {
-                for (String linha : linhas) {
-                    String[] campos = linha.split(",");
-                    if (campos[0].equals(professor.getCpf())) {
-                        writer.write(
-                                professor.getCpf() + "," +
-                                    professor.getNome() + "," +
-                                    professor.getAreaDeConhecimento() + "," +
-                                    professor.getPontos() + "\n"
-                                );
-                    } else {
-                        writer.write(linha + "\n");
-                    }
-                }
-            }
-        } catch (IOException e) {
-            System.err.println("Erro ao atualizar professor: " + e.getMessage());
-        }
+    public void atualizarProfessor(Professor professorAtualizado){
+        listaProfessor.atualizar(professorAtualizado, p -> p.getCpf().equals(professorAtualizado.getCpf()));// chama a função atualizar da lista e passa o criterio
     }
 
     public void removerProfessor(String cpf) {
-        Path caminhoArquivo = Paths.get(PROFESSORES_FILE);
-        try {
-            var linhas = Files.readAllLines(caminhoArquivo);
-            try (FileWriter writer = new FileWriter(PROFESSORES_FILE)) {
-                for (String linha : linhas) {
-                    String[] campos = linha.split(",");
-                    if (!campos[0].equals(cpf)) {
-                        writer.write(linha + "\n");
-                    }
-                }
-            }
-        } catch (IOException e) {
-            System.err.println("Erro ao remover professor: " + e.getMessage());
-        }
+        listaProfessor.remover(p -> p.getCpf().equals(cpf));// chama a função excluir da lista e passa o criterio
     }
 }
 
